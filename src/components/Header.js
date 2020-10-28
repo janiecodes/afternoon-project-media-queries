33346ac51e7e4f041270fb1afd2441ccd8a97a32
Janie Kim <janiekim@Janies-MBP.attlocal.net>
2020-10-28 18:30:04 -0500

    saving point

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,18 +15,15 @@ class Header extends Component{
             <header>
                 <div className="header-container">
                     <img className="bootstrap" src="https://startbootstrap.github.io/startbootstrap-agency/assets/img/navbar-logo.svg"/>
-                 
-                <nav>
-                    <div className="navigation-bar" text-uppercase ml-auto>
-                        <div className="navigation-items" href="#services">SERVICES</div>
-                        <div className="navigation-items" href="#portfolio">PORTFOLIO</div>
-                        <div className="navigation-items" href="#about">ABOUT</div>
-                        <div className="navigation-items" href="#team">TEAM</div>
-                        <div className="navigation-items" href="#contact">CONTACT</div>
-                    </div>
-                    <button className="hamburger">MENU &#9776;</button>
-                </nav>
+                    <nav>
+                            <div className="navigation-items" href="#services">SERVICES</div>
+                            <div className="navigation-items" href="#portfolio">PORTFOLIO</div>
+                            <div className="navigation-items" href="#about">ABOUT</div>
+                            <div className="navigation-items" href="#team">TEAM</div>
+                            <div className="navigation-items" href="#contact">CONTACT</div>
+                    </nav>
                 </div> 
+                <button className="hamburger">MENU &#9776;</button>
             </header>
         )
     }
